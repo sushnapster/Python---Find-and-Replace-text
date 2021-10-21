0a765b07128e3d28a84b346da202938a27942c46
Sushant K <34880848+sushnapster@users.noreply.github.com>
2021-10-21 14:04:41 +0530

    Update find_and_replace_double_quote_text.py

--- find_and_replace_double_quote_text.py
+++ find_and_replace_double_quote_text.py
@@ -1,15 +1,21 @@
-
-old_string= 'sushant"k'
-
+old_string= "sushant'k"
 if not old_string == None:
-	f_old=old_string.find('"')
-	print(f_old)
+	if old_string.find('"') > 0:
+		f_old=10
+		print(f_old)
+	elif old_string.find("'") > 0:
+		f_old=20
+		print(f_old)
 else:
 	f_old=0
 
-if f_old > 0:
+if f_old == 10:
 	new_string=old_string.replace('"','')
+	print(new_string)
+elif f_old == 20:
+	new_string=old_string.replace("'",'')
 	print(new_string)
 else:
 	old_string = old_string
 	print(old_string)
+    
